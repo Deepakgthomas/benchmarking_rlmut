714349aedb3c1af103cc38c1f26f8c66bf66eaaf
Deepakgthomas <thomasdeepak247@gmail.com>
2024-03-14 15:41:34 -0500

    Cleaned up repository

--- RLMutation/RLMT/test_python.py
+++
@@ -1,3 +0,0 @@
-import subprocess
-with open("stdout.txt", "w") as stdout, open("stderr.txt", "w") as stderr:
-    result = subprocess.run(["bash", "test.sh", "2"], stdout=stdout, stderr=stderr,text=True, check=True)
