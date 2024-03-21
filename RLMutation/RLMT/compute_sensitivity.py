c677dac253624881bea610e28e509a16a8e71edb
Deepakgthomas <thomasdeepak247@gmail.com>
2024-03-20 19:42:59 -0500

    Changed the ordering of rq2 tables to match paper

--- RLMutation/RLMT/compute_sensitivity.py
+++ RLMutation/RLMT/compute_sensitivity.py
@@ -42,26 +42,25 @@ for file_name in files:
             end_index=file_name.find("_stdout_")
             mutation_type = file_name[start_index + len("_weak_"):end_index]
 
-        operator_dict[str(algorithm)][str(environment)][str(test_generator_type)][str(mutation_type)] = defaultdict(int)
-        data_list.append((str(algorithm),str(environment), str(test_generator_type), str(mutation_type)))
+        operator_dict[str(environment)][str(algorithm)][str(test_generator_type)][str(mutation_type)] = defaultdict(int)
+        data_list.append((str(environment),str(algorithm), str(test_generator_type), str(mutation_type)))
         # Open the file for processing
         with open(file_path, 'r') as file:
             for line in file:
                 str_val = line.strip() #todo Not sure about line.strip(). Check this.
                 if "Operator" in str_val:
                     operator_value = str_val.replace("Operator", "").strip()
-                    # print("operator_value = ",operator_value)
                     ready_to_append = True
                 if "Distance Distribution Test" in str_val:
                     if ready_to_append:
                         mutation_result = str_val.replace("Distance Distribution Test :","").strip()
                         if "Not Killed" in mutation_result:
-                            operator_dict[str(algorithm)][str(environment)][str(test_generator_type)][str(mutation_type)][str(operator_value)]=0
+                            operator_dict[str(environment)][str(algorithm)][str(test_generator_type)][str(mutation_type)][str(operator_value)]=0
 
                         elif "Inconclusive" in mutation_result:
-                            operator_dict[str(algorithm)][str(environment)][str(test_generator_type)][str(mutation_type)][str(operator_value)]=0
+                            operator_dict[str(environment)][str(algorithm)][str(test_generator_type)][str(mutation_type)][str(operator_value)]=0
                         else:
-                            operator_dict[str(algorithm)][str(environment)][str(test_generator_type)][str(mutation_type)][str(operator_value)]=1 #todo Come up with a better way to represent killed
+                            operator_dict[str(environment)][str(algorithm)][str(test_generator_type)][str(mutation_type)][str(operator_value)]=1 #todo Come up with a better way to represent killed
 
                         ready_to_append = False
 
@@ -89,7 +88,6 @@ for i in data_list:
 
     weak_tuple=(str(i[0]),str(i[1]),str("weak"),str("constant"))
     sensitivity_dict[str(i[0])][str(i[1])][str(i[3])]=(result_df.at['mutation_score', i]-result_df.at['mutation_score', weak_tuple])/result_df.at['mutation_score', i]
-# Checking git commit
 sensitivity_df = pd.json_normalize(sensitivity_dict)
 sensitivity_df.columns = sensitivity_df.columns.str.split('.', expand=True) #Check this
 sensitivity_df.to_csv(str(rq2_folder)+"/sensitivity_df_.csv")
